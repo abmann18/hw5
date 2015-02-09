faba2dfd8f6ac184f7edde38fbd0dbbec609bbb4
abmann18 <ashley.b.mann@gmail.com>
2015-02-09 14:02:10 -0500

    Starting homework 5.

--- my_library.js
+++ my_library.js
@@ -1,8 +1,21 @@
-console.log(alert("Hello world!"));
+//console.log(alert("Hello world!"));
 
 var x = 5
 var y = 6
-console.log(x+y);
+/*console.log(x+y);
 
 var teas = ["white", "green", "black", "oolong"];
 console.log(teas [0]) + console.log(teas [3]);
+
+var apps = ["instagram", "zocdoc", "googlemaps"];
+console.log(apps[2]);*/
+
+var colors = ["yellow", "black", "green", "purple", "blue", "pink", "red"];
+console.log(alert(colors[1] + colors[3] + colors[5]));
+
+if(x > y){
+	console.log("No, 5 is not greater than 6.");
+}
+else{
+	console.log("Yes, 5 is less than 6.");
+}
